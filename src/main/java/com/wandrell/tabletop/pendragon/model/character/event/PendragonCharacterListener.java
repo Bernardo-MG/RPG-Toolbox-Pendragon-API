4b6eb382b24b8cfb825a82e3f3a6fd7be90fdcf6
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-10 15:45:56 +0100

    Added events for derived attributes to the Character listener.

--- src/main/java/com/wandrell/tabletop/pendragon/model/character/event/PendragonCharacterListener.java
+++ src/main/java/com/wandrell/tabletop/pendragon/model/character/event/PendragonCharacterListener.java
@@ -10,10 +10,28 @@ public interface PendragonCharacterListener extends EventListener {
 
     public void constitutionChanged(final ValueChangeEvent event);
 
+    public void damageChanged(final ValueChangeEvent event);
+
     public void dexterityChanged(final ValueChangeEvent event);
+
+    public void dexterityRollChanged(final ValueChangeEvent event);
+
+    public void healingRateChanged(final ValueChangeEvent event);
+
+    public void hitPointsChanged(final ValueChangeEvent event);
+
+    public void knockdownChanged(final ValueChangeEvent event);
+
+    public void majorWoundChanged(final ValueChangeEvent event);
+
+    public void moveRateChanged(final ValueChangeEvent event);
 
     public void sizeChanged(final ValueChangeEvent event);
 
     public void strengthChanged(final ValueChangeEvent event);
+
+    public void unconciousChanged(final ValueChangeEvent event);
+
+    public void weightChanged(final ValueChangeEvent event);
 
 }
--- src/main/java/com/wandrell/tabletop/pendragon/model/character/event/PendragonCharacterListenerAdapter.java
+++ src/main/java/com/wandrell/tabletop/pendragon/model/character/event/PendragonCharacterListenerAdapter.java
@@ -12,12 +12,39 @@ public class PendragonCharacterListenerAdapter implements
     public void constitutionChanged(final ValueChangeEvent event) {}
 
     @Override
+    public void damageChanged(final ValueChangeEvent event) {}
+
+    @Override
     public void dexterityChanged(final ValueChangeEvent event) {}
+
+    @Override
+    public void dexterityRollChanged(final ValueChangeEvent event) {}
+
+    @Override
+    public void healingRateChanged(final ValueChangeEvent event) {}
+
+    @Override
+    public void hitPointsChanged(final ValueChangeEvent event) {}
+
+    @Override
+    public void knockdownChanged(final ValueChangeEvent event) {}
+
+    @Override
+    public void majorWoundChanged(final ValueChangeEvent event) {}
+
+    @Override
+    public void moveRateChanged(final ValueChangeEvent event) {}
 
     @Override
     public void sizeChanged(final ValueChangeEvent event) {}
 
     @Override
     public void strengthChanged(final ValueChangeEvent event) {}
+
+    @Override
+    public void unconciousChanged(final ValueChangeEvent event) {}
+
+    @Override
+    public void weightChanged(final ValueChangeEvent event) {}
 
 }
