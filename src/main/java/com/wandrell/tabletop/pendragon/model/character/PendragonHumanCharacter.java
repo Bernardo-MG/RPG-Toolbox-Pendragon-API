bcbfa1d6cc61607720aaf374c5b115791c1ddf63
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-23 22:21:44 +0100

    Moved Traits to their own container class.

--- src/main/java/com/wandrell/tabletop/pendragon/model/character/PendragonHumanCharacter.java
+++ src/main/java/com/wandrell/tabletop/pendragon/model/character/PendragonHumanCharacter.java
@@ -5,9 +5,12 @@ import java.util.Collection;
 import com.wandrell.tabletop.character.Gender;
 import com.wandrell.tabletop.pendragon.model.character.background.Religion;
 import com.wandrell.tabletop.pendragon.model.stats.SpecialtySkillBox;
+import com.wandrell.tabletop.pendragon.model.stats.TraitsHolder;
 import com.wandrell.tabletop.valuebox.SkillBox;
 
 public interface PendragonHumanCharacter extends PendragonBaseCharacter {
+    
+    public TraitsHolder getTraits();
 
     public void addDirectedTrait(final SkillBox directedTrait);
 
@@ -30,79 +33,28 @@ public interface PendragonHumanCharacter extends PendragonBaseCharacter {
 
     public Integer getAppearance();
 
-    public Integer getArbitrary();
-
-    public Integer getChaste();
-
-    public Integer getCowardly();
-
-    public Integer getCruel();
-
     public String getCulture();
-
-    public Integer getDeceitful();
 
     public Collection<SkillBox> getDirectedTraits();
 
     public Collection<DistinctiveFeature> getDistinctiveFeatures();
 
-    public Integer getEnergetic();
-
     public String getFatherClass();
 
-    public Integer getForgiving();
 
     public Gender getGender();
-
-    public Integer getGenerous();
 
     public Integer getGlory();
 
     public String getHomeland();
 
-    public Integer getHonest();
-
-    public Integer getIndulgent();
-
-    public Integer getJust();
-
-    public Integer getLazy();
-
-    public Integer getLustful();
-
-    public Integer getMerciful();
-
-    public Integer getModest();
-
     public Collection<SkillBox> getPassions();
-
-    public Integer getPious();
 
     public String getPlayerName();
 
-    public Integer getProud();
-
-    public Integer getPrudent();
-
-    public Integer getReckless();
-
     public Religion getReligion();
 
-    public Integer getSelfish();
-
     public Collection<SpecialtySkillBox> getSpecialtySkills();
-
-    public Integer getSuspicious();
-
-    public Integer getTemperate();
-
-    public Integer getTrusting();
-
-    public Integer getValorous();
-
-    public Integer getVengeful();
-
-    public Integer getWorldly();
 
     public Boolean isKnight();
 
@@ -116,67 +68,19 @@ public interface PendragonHumanCharacter extends PendragonBaseCharacter {
 
     public void setAppearance(final Integer appearance);
 
-    public void setArbitrary(final Integer arbitrary);
-
-    public void setChaste(final Integer chaste);
-
-    public void setCowardly(final Integer cowardly);
-
-    public void setCruel(final Integer cruel);
-
-    public void setDeceitful(final Integer deceitful);
-
     public void setDirectedTraits(final Collection<SkillBox> directedTraits);
 
     public void setDistinctiveFeatures(
             final Collection<DistinctiveFeature> features);
 
-    public void setEnergetic(final Integer energetic);
-
-    public void setForgiving(final Integer forgiving);
-
-    public void setGenerous(final Integer generous);
-
     public void setGlory(final Integer glory);
-
-    public void setHonest(final Integer honest);
-
-    public void setIndulgent(final Integer indulgent);
-
-    public void setJust(final Integer just);
 
     public void setKnight(final Boolean knight);
 
-    public void setLazy(final Integer lazy);
-
-    public void setLustful(final Integer lustful);
-
-    public void setMerciful(final Integer merciful);
-
-    public void setModest(final Integer modest);
-
     public void setPassions(final Collection<SkillBox> passions);
-
-    public void setPious(final Integer pious);
-
-    public void setProud(final Integer proud);
-
-    public void setPrudent(final Integer prudent);
-
-    public void setReckless(final Integer reckless);
 
     public void setReligion(final Religion religion);
 
-    public void setSelfish(final Integer selfish);
-
     public void setSpecialtySkills(final Collection<SpecialtySkillBox> skills);
-
-    public void setSuspicious(final Integer suspicious);
-
-    public void setTemperate(final Integer temperate);
-
-    public void setTrusting(final Integer trusting);
-
-    public void setValorous(final Integer valorous);
 
 }
---
+++ src/main/java/com/wandrell/tabletop/pendragon/model/stats/TraitsHolder.java
@@ -0,0 +1,104 @@
+package com.wandrell.tabletop.pendragon.model.stats;
+
+public interface TraitsHolder {
+
+    public Integer getArbitrary();
+
+    public Integer getChaste();
+
+    public Integer getCowardly();
+
+    public Integer getCruel();
+
+    public Integer getDeceitful();
+
+    public Integer getEnergetic();
+
+    public Integer getGenerous();
+
+    public Integer getHonest();
+
+    public Integer getIndulgent();
+
+    public Integer getJust();
+
+    public Integer getLazy();
+
+    public Integer getLustful();
+
+    public Integer getMerciful();
+
+    public Integer getModest();
+
+    public Integer getPious();
+
+    public Integer getProud();
+
+    public Integer getPrudent();
+
+    public Integer getReckless();
+
+    public Integer getSelfish();
+
+    public Integer getSuspicious();
+
+    public Integer getTemperate();
+
+    public Integer getTrusting();
+
+    public Integer getValorous();
+
+    public Integer getVengeful();
+
+    public Integer getWorldly();
+
+    public void setArbitrary(final Integer arbitrary);
+
+    public void setChaste(final Integer chaste);
+
+    public void setCowardly(final Integer cowardly);
+
+    public void setCruel(final Integer cruel);
+
+    public void setDeceitful(final Integer deceitful);
+
+    public void setEnergetic(final Integer energetic);
+
+    public void setForgiving(final Integer forgiving);
+
+    public void setGenerous(final Integer generous);
+
+    public void setHonest(final Integer honest);
+
+    public void setIndulgent(final Integer indulgent);
+
+    public void setJust(final Integer just);
+
+    public void setLustful(final Integer lustful);
+
+    public void setMerciful(final Integer merciful);
+
+    public void setModest(final Integer modest);
+
+    public void setPious(final Integer pious);
+
+    public void setProud(final Integer proud);
+
+    public void setPrudent(final Integer prudent);
+
+    public void setReckless(final Integer reckless);
+
+    public void setSelfish(final Integer selfish);
+
+    public void setSuspicious(final Integer suspicious);
+
+    public void setTemperate(final Integer temperate);
+
+    public void setTrusting(final Integer trusting);
+
+    public void setValorous(final Integer valorous);
+
+    public Integer getForgiving();
+
+    public void setLazy(final Integer lazy);
+}
