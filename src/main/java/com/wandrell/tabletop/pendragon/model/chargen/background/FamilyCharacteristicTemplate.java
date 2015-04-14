bc42e2d88fffa3461ae6ebbd5aa910c039dfcbf4
Bernardo Martínez Garrido <programming@wandrell.com>
2015-04-14 23:40:44 +0200

    Corrected a return type.

--- src/main/java/com/wandrell/tabletop/pendragon/model/chargen/background/FamilyCharacteristicTemplate.java
+++ src/main/java/com/wandrell/tabletop/pendragon/model/chargen/background/FamilyCharacteristicTemplate.java
@@ -3,7 +3,7 @@ package com.wandrell.tabletop.pendragon.model.chargen.background;
 import java.util.Collection;
 
 import com.wandrell.pattern.prototype.Prototype;
-import com.wandrell.tabletop.pendragon.model.character.PendragonCharacter;
+import com.wandrell.tabletop.pendragon.model.character.stats.AttributesHolder;
 import com.wandrell.tabletop.valuebox.SkillBox;
 
 public interface FamilyCharacteristicTemplate extends Prototype {
@@ -11,7 +11,7 @@ public interface FamilyCharacteristicTemplate extends Prototype {
     @Override
     public FamilyCharacteristicTemplate createNewInstance();
 
-    public PendragonCharacter getAttributes();
+    public AttributesHolder getAttributes();
 
     public String getName();
 
