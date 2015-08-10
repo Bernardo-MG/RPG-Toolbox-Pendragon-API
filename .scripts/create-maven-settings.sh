a4b30937c89add4d2d469827385743e9203d5d3c
Bernardo Martínez Garrido <programming@wandrell.com>
2015-08-10 14:26:57 +0200

    Updated scripts.

--- .scripts/create-maven-settings.sh
+++ .scripts/create-maven-settings.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-# This script creates the Maven settings file
-# This includes the connection settings, which are loaded from the server's environment
+# This script creates the Maven settings file for deployment.
+#
+# This includes the connection settings, which are loaded from the server's environment variables.
+#
+# The data generated during this script should not be shared. Don't print it on the console.
+#
+# The following environmental variables are used:
+# - DEPLOY_USER: string, user for the releases repo
+# - DEPLOY_PASSWORD: string, password for the releases repo
+# - DEPLOY_DEVELOP_USER: string, user for the development repo
+# - DEPLOY_DEVELOP_PASSWORD: string, password for the development repo
+# - DEPLOY_DOCS_USER: string, user for the releases documentation site repo
+# - DEPLOY_DOCS_PASSWORD: string, password for the releases documentation site repo
+# - DEPLOY_DOCS_DEVELOP_USER: string, user for the development documentation site repo
+# - DEPLOY_DOCS_DEVELOP_PASSWORD: string, password for the development documentation site repo
+# - SCM_BRANCH: string, the CMS branch from which the code has been taken
 
 {
    echo "<settings>";
@@ -8,33 +22,33 @@
    echo "<servers>";
    # Releases FTP
    echo "<server>";
-      echo "<id>\${env.FTP_RELEASES_ID}</id>";
-      echo "<username>\${env.FTP_RELEASES_USER}</username>";
-      echo "<password>\${env.FTP_RELEASES_PASSWORD}</password>";
+      echo "<id>releases</id>";
+      echo "<username>\${env.DEPLOY_USER}</username>";
+      echo "<password>\${env.DEPLOY_PASSWORD}</password>";
    echo "</server>";
-   # Snapshots FTP
+   # DEVELOP FTP
    echo "<server>";
-      echo "<id>\${env.FTP_SNAPSHOTS_ID}</id>";
-      echo "<username>\${env.FTP_SNAPSHOTS_USER}</username>";
-      echo "<password>\${env.FTP_SNAPSHOTS_PASSWORD}</password>";
+      echo "<id>snapshots</id>";
+      echo "<username>\${env.DEPLOY_DEVELOP_USER}</username>";
+      echo "<password>\${env.DEPLOY_DEVELOP_PASSWORD}</password>";
    echo "</server>";
    # Site FTP
    echo "<server>";
-      echo "<id>\${env.FTP_SITE_ID}</id>";
-      echo "<username>\${env.FTP_SITE_USER}</username>";
-      echo "<password>\${env.FTP_SITE_PASSWORD}</password>";
+      echo "<id>site</id>";
+      echo "<username>\${env.DEPLOY_DOCS_USER}</username>";
+      echo "<password>\${env.DEPLOY_DOCS_PASSWORD}</password>";
    echo "</server>";
    # Development site FTP
    echo "<server>";
-      echo "<id>\${env.FTP_SITE_DEVELOP_ID}</id>";
-      echo "<username>\${env.FTP_SITE_DEVELOP_USER}</username>";
-      echo "<password>\${env.FTP_SITE_DEVELOP_PASSWORD}</password>";
+      echo "<id>site-development</id>";
+      echo "<username>\${env.DEPLOY_DOCS_DEVELOP_USER}</username>";
+      echo "<password>\${env.DEPLOY_DOCS_DEVELOP_PASSWORD}</password>";
    echo "</server>";
    
    echo "</servers>";
    
    # Active profile
-   if [ "$TRAVIS_BRANCH" == "develop" ]; then
+   if [ "$SCM_BRANCH" == "develop" ]; then
       echo "<activeProfiles>"
          echo "<activeProfile>development</activeProfile>"
       echo "</activeProfiles>"
--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-# This script deploys the application using the POM configuration
-# It is triggered only commits to the master or develop branches. Pulls are ignored
-# Also, it will only deploy on a concrete JDK version
+# This script deploys the project artifact.
+#
+# Make sure you have the deployment configuration ready before using it.
+#
+# It is triggered only commits to the master or develop branches, ignoring pulls.
+#
+# Also, it will only deploy if the DEPLOY environment variable is set to 'true'.
+#
+# The following environmental variables are used:
+# - PULL_REQUEST: boolean, indicates if this is a pull request, should be false for deployment
+# - DEPLOY: boolean, control flag for deployment, should be true to deploy
+# - SCM_BRANCH: string, the CMS branch from which the code has been taken
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_JDK_VERSION" == "$JDK_DEPLOY" ] && [[ "$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" == "develop" ]]; then
+if [ "$PULL_REQUEST" == "false" ] && [ "$DEPLOY" == "true" ] && [[ "$SCM_BRANCH" == "master" || "$SCM_BRANCH" == "develop" ]]; then
 
    echo "Deploying Java artifact to repository"
 
    mvn deploy --settings ~/settings.xml
-
+   
 else
 
    echo "Java artifact won't be deployed to repository"
